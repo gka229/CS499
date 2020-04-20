bfde0f6f7143828e40eb8ead17fdb0bebe7198f4
Guerschom M kayembe <45473300+gka229@users.noreply.github.com>
2020-04-19 22:40:49 -0400

    First commit

---
+++ iOSApp/UKSmartParking/Rules.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UKSmartParking
+{
+    public partial class Rules : ContentPage
+    {
+        public Rules()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+        async void About_App(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new AboutApp());
+        }
+        async void Campus_Map(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new CampusMap());
+        }
+        async void Parking_Regulation(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new ParkingRegulations());
+        }
+
+
+    }
+}
