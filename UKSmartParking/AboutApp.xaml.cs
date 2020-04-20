306b0fae9706b34daa70adc93c3b9be03f991ee1
Guerschom M kayembe <45473300+gka229@users.noreply.github.com>
2020-04-19 21:44:16 -0400

    First commit

---
+++ UKSmartParking/AboutApp.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UKSmartParking
+{
+    public partial class AboutApp : ContentPage
+    {
+        public AboutApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
